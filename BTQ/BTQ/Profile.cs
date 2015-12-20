c36241061e337ff0ee4327ae4135aab8d7a22107
Peter Young <pyoung0501@gmail.com>
2015-12-20 17:07:45 -0600

    Created initial Profile class and unit tests
    Created unit tests for creating a profile and setting its name.  Created
    the initial Profile test.

---
+++ BTQ/BTQ/Profile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTQ
+{
+    /// <summary>
+    /// Profile associated with a user.  A profile will contain or be
+    /// linked to accounts.
+    /// </summary>
+    public class Profile
+    {
+        /// <summary>
+        /// Name of the profile.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Constructs a new instance of Profile.
+        /// </summary>
+        public Profile()
+        {
+            Name = "";
+        }
+    }
+}
---
+++ BTQ/BTQUnitTests/ProfileTests.cs
@@ -0,0 +1,49 @@
+﻿using BTQ;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BTQUnitTests
+{
+    [TestClass]
+    public class ProfileTests
+    {
+        [TestMethod]
+        public void creating_a_profile_creates_a_profile_with_no_name()
+        {
+            // Arrange
+
+            // Act
+            Profile profile = new Profile();
+
+            // Assert
+            Assert.AreEqual("", profile.Name);
+        }
+
+        [TestMethod]
+        public void setting_name_on_a_profile_changes_its_name()
+        {
+            // Arrange
+            Profile profile = new Profile();
+            string NameToSetOnProfile = "Fred";
+
+            // Act
+            profile.Name = NameToSetOnProfile;
+
+            // Assert
+            Assert.AreEqual(NameToSetOnProfile, profile.Name);
+        }
+
+        [TestMethod]
+        public void getting_name_on_a_profile_returns_its_name()
+        {
+            // Arrange
+            string InitialProfileName = "Fred";
+            Profile profile = new Profile() { Name = InitialProfileName };
+
+            // Act
+            string profileName = profile.Name;
+
+            // Assert
+            Assert.AreEqual(InitialProfileName, profileName);
+        }
+    }
+}
