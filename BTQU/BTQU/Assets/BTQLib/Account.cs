925af70e35c6720d19e658fb5bf1a14282a60e5b
Peter Young <pyoung0501@gmail.com>
2016-03-26 15:22:59 -0500

    Unity Test of BTQ.

---
+++ BTQU/BTQU/Assets/BTQLib/Account.cs
@@ -0,0 +1,27 @@
+﻿namespace BTQ
+{
+    /// <summary>
+    /// A financial account containing transactions.
+    /// </summary>
+    public class Account
+    {
+        public string ID { get; set; }
+
+        /// <summary>
+        /// Constructs an Account without an ID.
+        /// </summary>
+        public Account()
+        {
+            ID = "";
+        }
+
+        /// <summary>
+        /// Constructs an Account with the given ID.
+        /// </summary>
+        /// <param name="id">ID of account.</param>
+        public Account(string id)
+        {
+            ID = id;
+        }
+    }
+}
