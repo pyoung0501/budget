98874bffc30947ea85f6f0469d8ea8f2fd640539
Peter Young <pyoung0501@gmail.com>
2016-07-24 15:42:03 -0500

    Created columns for displaying the applied to state and applied to category fields of income transactions.

---
+++ BTQU/BTQU/Assets/Scripts/Editor/Columns/AppliedToColumn.cs
@@ -0,0 +1,41 @@
+﻿using BTQ;
+using BTQLib;
+using System;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Defines the column for displaying the applied to state
+/// for income transactions.
+/// </summary>
+public class AppliedToColumn: TransactionColumn
+{
+    /// <summary>
+    /// Display name of the column.
+    /// </summary>
+    public override string DisplayName { get { return "Applied To"; } }
+
+    /// <summary>
+    /// Draws the "applied to" of the given income transaction. If this is not
+    /// an income transaction, an error will be drawn instead.
+    /// </summary>
+    /// <param name="transaction">Transaction to draw.</param>
+    public override void Draw(Transaction transaction)
+    {
+        if(transaction.Amount < 0)
+        {
+            GUI.enabled = false;
+            EditorGUILayout.TextField("!!! Non-Income !!!");
+            GUI.enabled = true;
+            return;
+        }
+
+        AppliedState prevVal = transaction.AppliedState;
+        AppliedState newVal = (AppliedState)EditorGUILayout.EnumPopup(prevVal, GUILayout.Width(Width));
+
+        if (newVal != prevVal)
+        {
+            transaction.AppliedState = newVal;
+        }
+    }
+}
---
+++ BTQU/BTQU/Assets/Scripts/Editor/Columns/CategoryAppliedToColumn.cs
@@ -0,0 +1,65 @@
+﻿using BTQLib;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Defines the column for displaying the category an income transaction
+/// is applied to if it is applied to a specific category.
+/// </summary>
+public class CategoryAppliedToColumn : TransactionColumn
+{
+    /// <summary>
+    /// Reference to the transaction categories for an account.
+    /// </summary>
+    private Categories _categories;
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="categories">An account's transaction categories.</param>
+    public CategoryAppliedToColumn(Categories categories)
+    {
+        _categories = categories;
+    }
+
+    /// <summary>
+    /// The display name of the column.
+    /// </summary>
+    public override string DisplayName { get { return "Applied Category"; } }
+
+    /// <summary>
+    /// Draws the category the given income transaction applies to.
+    /// </summary>
+    /// <param name="transaction">Transaction to draw.</param>
+    public override void Draw(Transaction transaction)
+    {
+        if (transaction.Amount < 0)
+        {
+            GUI.enabled = false;
+            EditorGUILayout.TextField("!!! Non-Income !!!");
+            GUI.enabled = true;
+            return;
+        }
+
+        EditorGUILayout.BeginHorizontal(GUILayout.Width(Width));
+        {
+            if (transaction.AppliedState != AppliedState.ApplyToCategory)
+            {
+                GUI.enabled = false;
+                EditorGUILayout.TextField("");
+                GUI.enabled = true;
+            }
+            else
+            {
+                string prevCategory = transaction.AppliedToCategory;
+                string currCategory = EditorGUILayout.TextField(prevCategory);
+                if (currCategory != null)
+                {
+                    string primaryCategory = currCategory;
+                    transaction.AppliedToCategory = primaryCategory;
+                }
+            }
+        }
+        EditorGUILayout.EndHorizontal();
+    }
+}
